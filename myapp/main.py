cfe6733514fe89dab2a55394a6cd39d363db52bc
donald-besong <donbes@yahoo.com>
2019-12-19 11:42:36 +0000

    added the amended files

--- myapp/main.py
+++ myapp/main.py
@@ -1,53 +1,55 @@
 import json
-import other_code as other_code
 from bokeh.io import curdoc
 from bokeh.models import Button
 import os
 import sys
+import imp
 
 file_dir = os.path.dirname(os.path.abspath(__file__))
 working_dir = os.getcwd()
-sys.path.append(working_dir + "\OTHER")
+path_to_other = working_dir + "\OTHER\other_code.py"
+other_code_local = imp.load_source('', path_to_other)
+print(other_code_local.number().age)
 
-print("Loading function ......")
-def lambda_handler(event, context):
-    # TODO implement
-    number_obj = other_code.number("Donald")
-    name = number_obj.name
-    age = number_obj.age
-    event_key1 = event['key1']
-    print(other_code.x)
-    print("value1 = " + event.get("key1"))
-    print("hello there. This is a test")
-    print("My name is = " + name)
-    print("My age is = " + str(age))
-    return {  #this return will only appear if I reun in within AWS
-        'my name': name,
-        'my age': age,
-        'statusCode': 200,
-        'event': event_key1,
-        'body': json.dumps('Hello from Lambda!')
-    }
-
-if __name__ == "__main__":
-    class Event:
-        def __init__(self):
-             self.e = { "key1": "key1_value",
-                  "key2": "key2_value",
-                  "key3": "key3_value"
-                  }
-        def get(self, key):
-            return(self.e[key])
-    event_obj = Event()
-    event = event_obj.e
-    context ="arbitrary context"
-    lambda_handler(event, context)
-    
-bt = Button(label=other_code.x)
-#doc=curdoc()
-#doc.add_root(bt)
-
-#4. For Bokeh Server: Displaying the document using (note Microsft edge does not work well. Use Chrome)
-#on the command line, run "bokeh serve --show main.py"
-
-#5, In heroku, debug with "heroku logs --tail --app test-herokuapp-instance"
+#print("Loading function ......")
+#def lambda_handler(event, context):
+#    # TODO implement
+#    number_obj = other_code.number("Donald")
+#    name = number_obj.name
+#    age = number_obj.age
+#    event_key1 = event['key1']
+#    print(other_code.x)
+#    print("value1 = " + event.get("key1"))
+#    print("hello there. This is a test")
+#    print("My name is = " + name)
+#    print("My age is = " + str(age))
+#    return {  #this return will only appear if I reun in within AWS
+#        'my name': name,
+#        'my age': age,
+#        'statusCode': 200,
+#        'event': event_key1,
+#        'body': json.dumps('Hello from Lambda!')
+#    }
+#
+#if __name__ == "__main__":
+#    class Event:
+#        def __init__(self):
+#             self.e = { "key1": "key1_value",
+#                  "key2": "key2_value",
+#                  "key3": "key3_value"
+#                  }
+#        def get(self, key):
+#            return(self.e[key])
+#    event_obj = Event()
+#    event = event_obj.e
+#    context ="arbitrary context"
+#    lambda_handler(event, context)
+#    
+#bt = Button(label=other_code.x)
+##doc=curdoc()
+##doc.add_root(bt)
+#
+##4. For Bokeh Server: Displaying the document using (note Microsft edge does not work well. Use Chrome)
+##on the command line, run "bokeh serve --show main.py"
+#
+##5, In heroku, debug with "heroku logs --tail --app test-herokuapp-instance"
